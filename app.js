9bb3425bc9016020c14363f539d6947fa001ae73
hjl19910901 <18710360575@163.com>
2019-07-03 21:29:44 +0800

    博客开发

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // 应用程序的启动文件
 var express =require("express");   //加载express模块
 //创建app应用
+
+//加载模板
+var swig=require('swing');
 var app=express();
+
+// 配置应用模板
+
+//定义当前应用所使用的模板引擎
+app.engine('html',swig.renderFile);
+
 /*
 req request对象
 res response对象
