6f7bcff8870f587a4c5d3a2bb5a8b48160868dab
hjl19910901 <18710360575@163.com>
2019-07-03 21:21:37 +0800

    博客开发

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 var express =require("express");   //加载express模块
 //创建app应用
 var app=express();
+/*
+req request对象
+res response对象
+next 函数
+
+*/
+app.get('/',function (req,res,next) {
+    res.send('<h1>欢迎光临我的博客!</h1>');
+})
 
 //监听http请求
 app.listen(8081);
