d4848ef0c7f7df7dd4267a5a972048b12ec60c35
hjl19910901 <18710360575@163.com>
2019-07-03 21:41:10 +0800

    博客开发

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ var app=express();
 // 配置应用模板
 
 //定义当前应用所使用的模板引擎
+// 第一个参数：模板引擎的名称，同时也是模板引擎的后缀，第二个参数表示用于解析处理模板内容的方法
 app.engine('html',swig.renderFile);
+// 设置模板文件存放的目录,第一个参数必须是view，第二个参数是目录
+app.set('views','./views');
+
+// 注册所使用的模板引擎,第一个参数必须是view engine,第二个参数和app.engine这个方法中定义的模板引擎的名称（第一个参数）是一致的
+app.set('view engine','html');
+
 
 /*
 req request对象
